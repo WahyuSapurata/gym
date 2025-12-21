afdb6d2ff68c071928f2c60c688614ebe1a27962
WahyuSapurata <wm337708@gmail.com>
2025-12-21 18:55:47 +0800

    update sesi

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -499,7 +499,7 @@ class TransaksiController extends BaseController
         ];
 
         // ✅ Tambahkan sesi HANYA jika ada
-        if ($transaksi->remaining_session !== 0 && $transaksi->remaining_session !== null) {
+        if ($transaksi->remaining_session !== 0) {
             $labels['Sisa Sesi'] = $transaksi->remaining_session;
         }
 
